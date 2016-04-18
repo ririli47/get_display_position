f7ff87c295bba66e2e2c30872fe3c4592384bdf4
Naoya Fukuhara <suguha47@gmail.com>
2016-04-18 14:24:41 +0900

    new

---
+++ get_display_position/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace get_display_position
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(x,y) =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "(Width, Height) = ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ get_display_position/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace get_display_position
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x, y;
+            string x_string, y_string;
+
+            x = MousePosition.X;
+            y = MousePosition.Y;
+
+            x_string = System.Convert.ToString(x);
+            y_string = System.Convert.ToString(y);
+            label1.Text = "(x,y) = " + x_string + "," + y_string;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int width, height;
+            string width_string, height_string;
+
+            /*
+            width = this.Size.Width;
+            height = this.Size.Height;
+            */
+
+            width = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width;
+            height = System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height;
+
+            width_string = System.Convert.ToString(width);
+            height_string = System.Convert.ToString(height);
+
+            label2.Text = "(Width, Height) = " + width_string + "," + height_string;
+
+            draw_circle();
+        }
+
+        private System.Windows.Forms.Label[] labels;
+
+        public void draw_circle()
+        {
+            int x = 0;
+            int i, j;
+            int cc = 0;
+
+            for (i = 0; i < System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width; i++)
+            {
+                for (j = 0; j < System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height; j++)
+                {
+                    if ((i % 200 == 0) && (j % 200 == 0))
+                    {
+                        if (i == 0 || j == 0)
+                            ;
+                        else
+                            cc++;
+                    }
+                }
+            }
+            this.labels = new System.Windows.Forms.Label[cc];
+
+            for (i = 0; i < System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width; i++)
+            {
+                for (j = 0; j < System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height; j++)
+                {
+                    if ((i % 200 == 0) && (j % 200 == 0))
+                    {
+                        if (i == 0 || j == 0)
+                        {
+                            ;
+                        }
+                        else
+                        {
+                            this.labels[x] = new System.Windows.Forms.Label();
+
+                            this.labels[x].Name = "labels" + x.ToString();
+                            this.labels[x].Text = "●";
+                            this.labels[x].Size = new Size(15,15);
+                            this.labels[x].Location = new Point(i, j);
+
+                            x++;
+                        }
+                    }
+                }
+            }
+            this.Controls.AddRange(this.labels);
+        }
+    }
+}
